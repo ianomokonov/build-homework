fe8b22ce6411e6d4acaf3eebcb3a79ec9634d1f1
Ivan Nomokonov <nomokonovi@vtb.ru>
2025-08-19 12:55:52 +0300

    task 4

--- tasks/4/main.js
+++ tasks/4/main.js
@@ -4,12 +4,16 @@ import * as astring from "astring";
 import { parse } from "acorn";
 
 // read file with source code
+const code = fs.readFileSync('./entry.js', 'utf-8');
 
 // get ast from source code
+const ast = parse(code);
 
 // transform ast
+const transformed = transformer(ast);
 
 // convert ast to source code
+const result = astring.generate(transformed);
 
 // write source code to file
 fs.writeFileSync("./result.js", result);
--- tasks/4/transformer.js
+++ tasks/4/transformer.js
@@ -2,5 +2,15 @@ import { walk } from 'estree-walker'
 
 // should return transformed ast
 export function transformer(ast) {
-  
+  walk(ast, {
+    enter(node){
+        if(node.type === 'VariableDeclaration'){
+            if(node.kind === 'let' || node.kind === 'const'){
+                node.kind = 'var';
+            }
+        }
+    }
+  })
+
+  return ast;
 }
